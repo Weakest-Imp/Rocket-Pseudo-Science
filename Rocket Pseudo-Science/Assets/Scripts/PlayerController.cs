6bf6af89972222cd4d5577291ca0401713d2b2e9
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-08-25 11:25:29 +0200

    Initial commit
    A player moving left and right, and falling into oblivion.

---
+++ Rocket Pseudo-Science/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] float groundSpeed;
+	[SerializeField] float jumpForce;
+
+	float moveInput;
+
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		GroundMove ();
+	}
+
+
+	//How to move when grounded
+	void GroundMove ()
+	{
+		moveInput = Input.GetAxis ("Horizontal");
+		rb.velocity = new Vector2 (moveInput * groundSpeed, rb.velocity.y);
+	}
+}
