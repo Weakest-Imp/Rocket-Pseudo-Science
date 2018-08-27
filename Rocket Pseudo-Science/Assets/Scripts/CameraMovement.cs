85a7c613e521d630cb0a41420816b3968e1313c0
Zekotsu <36639546+Zekotsu@users.noreply.github.com>
2018-08-27 09:59:23 +0200

    First jump
    Character can jump once. No double jump yet.

---
+++ Rocket Pseudo-Science/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	[SerializeField] GameObject player;
+
+	void Update () {
+		float x = player.transform.position.x;
+		float y = player.transform.position.y;
+
+		float z = transform.position.z;
+		transform.position = new Vector3 (x, y, z);
+	}
+}
--- Rocket Pseudo-Science/Assets/Scripts/PlayerController.cs
+++ Rocket Pseudo-Science/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	[SerializeField] float groundSpeed;
-	[SerializeField] float jumpForce;
+	[SerializeField] private float groundSpeed = 10;
+	[SerializeField] private float jumpForce = 25;
+	[SerializeField] private float jumpBoost = 1.5f;
+	//Gravity is 10 G
 
-	float moveInput;
+	private float moveInput;
+	private float moveInputRaw;
+	private float jumpInput;
+	private bool facingRight = true;
+	private bool isGrounded;
+	[SerializeField] private Transform groundCheck1;
+	[SerializeField] private Transform groundCheck2;
+	[SerializeField] private float checkRadius = 0.144f;
+	[SerializeField] private LayerMask whatIsGround;
 
 	private Rigidbody2D rb;
 
@@ -16,14 +26,53 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		GroundMove ();
+		isGrounded = Grounded ();
+		if (isGrounded) {
+			GroundMove ();
+			JumpFromGround ();
+		}
 	}
 
 
-	//How to move when grounded
 	void GroundMove ()
+	//How to move when grounded
 	{
 		moveInput = Input.GetAxis ("Horizontal");
 		rb.velocity = new Vector2 (moveInput * groundSpeed, rb.velocity.y);
+		if (facingRight && moveInput < 0) {
+			//Facing rihght & going left
+			Flip ();
+		}
+		if (!facingRight && moveInput > 0) {
+			//Faing left & going right
+			Flip ();
+		}
+	}
+
+	void Flip () 
+	//Makes the character change the direction it looks
+	{
+		facingRight = !facingRight;
+		Vector3 Scaler = transform.localScale;
+		Scaler.x *= -1;
+		transform.localScale = Scaler;
+	}
+
+	bool Grounded ()
+	//returns whether the player is on the ground or not
+	{
+		bool ground1 = Physics2D.OverlapCircle (groundCheck1.position, checkRadius, whatIsGround);
+		bool ground2 = Physics2D.OverlapCircle (groundCheck2.position, checkRadius, whatIsGround);
+		return (ground1 || ground2);
+	}
+
+	void JumpFromGround () {
+		jumpInput = Input.GetAxisRaw ("Jump");
+		moveInputRaw = Input.GetAxisRaw ("Horizontal");
+		if (jumpInput > 0) {
+			float xVelocity = groundSpeed * moveInputRaw * jumpBoost;
+			float yVelocity = jumpForce;
+			rb.velocity = new Vector2 (xVelocity, yVelocity);
+		}
 	}
 }
